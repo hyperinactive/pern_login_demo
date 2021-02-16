e78e34bd6066e84dba1086186acbc9ed5c2ca035
Hyper <hyper@pop-os.localdomain>
2021-02-17 00:00:55 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+module.exports = app;
